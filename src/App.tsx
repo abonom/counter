ea634f0bc4f50bf126a0175319b59ae46ba2528a
abonom <andrej.bonomanko@gmail.com>
2021-12-03 21:54:15 +0100

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [counter, setCounter] = useState<number>(0)
+    //const [disable, setDisable] = useState<boolean>(false)
+
+    let isDisableIncr: boolean = false;
+    let isDisableSet: boolean = false;
+
+    const increaseCounter = () => {
+        setCounter(counter + 1)
+    }
+
+    const resetCounter = () => {
+        setCounter(0)
+    }
+
+    if (counter === 5) {
+        isDisableIncr = !isDisableIncr
+    }
+
+    if (counter === 0) {
+        isDisableSet = !isDisableSet
+    }
+
+    const isMaxValueClass = counter === 5 ? "maxValue" : ""
+
+    return (
+        <div className="App">
+            <div className={`display ${isMaxValueClass}`}>{counter}</div>
+            <div className="buttons">
+                <button onClick={increaseCounter} disabled={isDisableIncr}>incr</button>
+                <button onClick={resetCounter} disabled={isDisableSet}>reset</button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
